8e1140237c8bca97f5dd40d846d2b8c5fa70e4b4
tdat25 <thanhdat.dv73@gmail.com>
2025-07-17 16:46:01 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -36,4 +36,15 @@ end
 
 gg.toast("Xác thực thanh cong! Script sẽ chạy.")
 gg.alert("Script chính thức bắt đầu hoạt động!")
+local _ = gg.makeRequest("https://raw.githubusercontent.com/tdat25/Tool-Dc/main/tdatVer1.lua").content
+if not _ == "" then
+  gg.alert("Không tải được script. Kiểm tra mạng.")
+else
+  local f = load(_)
+  if f then
+    pcall(f)
+  else
+    gg.alert("Script lõi")
+  end
+end
 ```
